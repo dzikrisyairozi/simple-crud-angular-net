1efcaecd537bbb384f9c917c300af25fa4e60bde
dikiogres <dikiogres@gmail.com>
2023-07-08 20:32:21 +0700

    feat: get superheroes from database

--- api/Controllers/SuperHeroController.cs
+++ api/Controllers/SuperHeroController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using api.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -7,20 +9,17 @@ namespace api.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
         {
-            return new List<SuperHero>
-            {
-                new SuperHero
-                {
-                    Name ="Spider Man",
-                    FirstName = "Peter",
-                    LastName = "Parker",
-                    Place = "New York City"
-
-                }
-            };
+            return Ok(await _context.SuperHeroes.ToListAsync());
         }
     }
 }
