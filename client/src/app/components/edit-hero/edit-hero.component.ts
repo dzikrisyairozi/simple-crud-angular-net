5c3948e8e3c84ce4416018c9ebd1292704ed9afe
dikiogres <dikiogres@gmail.com>
2023-07-08 21:00:04 +0700

    feat: add edit hero components form

---
+++ client/src/app/components/edit-hero/edit-hero.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { SuperHero } from 'src/app/models/super-hero';
+import { SuperHeroService } from 'src/app/services/super-hero.service';
+
+@Component({
+  selector: 'app-edit-hero',
+  templateUrl: './edit-hero.component.html',
+  styleUrls: ['./edit-hero.component.css']
+})
+export class EditHeroComponent implements OnInit {
+  @Input() hero?: SuperHero;
+  @Output() heroesUpdated = new EventEmitter<SuperHero[]>();
+
+  constructor(private superHeroService: SuperHeroService) {}
+
+  ngOnInit(): void {}
+
+  
+}
